5eb6af30a48454a4a498816910034da7eb4901ad
Bill Tran <bill@billjohntran.com>
2019-02-03 22:32:44 -0800

    add logo launch screen and animation

--- screens/QuestionScreen/QuestionScreen.js
+++ screens/QuestionScreen/QuestionScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'react-native-elements';
 
 import { AllHtmlEntities as Entities } from 'html-entities';
-import { Text, View } from 'react-native';
+import { Text, View, Animated, Dimensions } from 'react-native';
 import styles from '../../styles';
 
 const entities = new Entities();
+const { width, height } = Dimensions.get('window');
 
 const getBackgroundColor = () => {
   const colors = [
@@ -25,99 +26,144 @@ const getBackgroundColor = () => {
   return colors[Math.floor(Math.random() * Math.floor(colors.length))];
 };
 
-const QuestionScreen = ({ currentQuestion, nextQuestion, componentId }) => (
-  <View
-    style={{
-      ...styles.font,
-      flex: 1,
-      backgroundColor: getBackgroundColor(),
-      alignItems: 'center',
-    }}
-  >
-    <Text
-      style={{
-        ...styles.font,
-        marginTop: 50,
-        fontSize: 30,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        position: 'absolute',
-        top: 0,
-      }}
-    >
-      {currentQuestion.category}
-    </Text>
+class QuestionScreen extends Component {
+  componentWillUpdate() {
+    this.animatedValue1 = new Animated.Value(0);
+    this.animatedValue2 = new Animated.Value(width);
 
-    <Text
-      style={{
-        ...styles.font,
-        marginTop: 150,
-        fontSize: 20,
-        textAlign: 'center',
-      }}
-    >
-      {currentQuestion.index > -1 ? `${currentQuestion.index + 1} of 10` : ''}
-    </Text>
+    Animated.timing(this.animatedValue1, {
+      toValue: 1,
+      duration: 1500,
+    }).start();
 
-    <View
-      style={{
-        ...styles.font,
-        marginTop: 20,
-        width: 300,
-        height: 300,
-      }}
-    >
-      <Text
+    Animated.timing(this.animatedValue2, {
+      toValue: 1,
+      duration: 1500,
+    }).start();
+  }
+
+  nextQuestion(answer, componentId) {
+    const { nextQuestion } = this.props;
+
+    nextQuestion(answer, componentId);
+  }
+
+  render() {
+    const { componentId, currentQuestion } = this.props;
+    const animatedStyle1 = { opacity: this.animatedValue1 };
+    const animatedStyle2 = { left: this.animatedValue2 };
+
+    return (
+      <View
         style={{
           ...styles.font,
-          fontSize: 30,
-          padding: 20,
-          textAlign: 'center',
+          flex: 1,
+          backgroundColor: getBackgroundColor(),
+          alignItems: 'center',
         }}
       >
-        {entities.decode(currentQuestion.question)}
-      </Text>
-    </View>
-    <View
-      style={{ ...styles.font, marginTop: 20, flex: 1, flexDirection: 'row' }}
-    >
-      <View
-        style={{
-          margin: 30,
-        }}
-      >
-        <Icon
-          raised
-          name="thumbs-up"
-          type="font-awesome"
-          color="#63cdda"
-          reverse
-          size={30}
-          onPress={() => {
-            nextQuestion('True', componentId);
-          }}
-        />
+        <Animated.View
+          style={[
+            animatedStyle1,
+            {
+              marginTop: 50,
+              fontWeight: 'bold',
+              position: 'absolute',
+              top: 0,
+            },
+          ]}
+        >
+          <Text style={[styles.font, { fontSize: 30, textAlign: 'center' }]}>
+            {currentQuestion.category}
+          </Text>
+        </Animated.View>
+
+        <Animated.View style={[animatedStyle2]}>
+          <Text
+            style={{
+              ...styles.font,
+              marginTop: 150,
+              fontSize: 20,
+              textAlign: 'center',
+            }}
+          >
+            {currentQuestion.index > -1
+              ? `${currentQuestion.index + 1} of 10`
+              : ''}
+          </Text>
+        </Animated.View>
+
+        <Animated.View
+          style={[
+            styles.font,
+            animatedStyle2,
+            {
+              marginTop: 20,
+              width: 300,
+              height: 300,
+            },
+          ]}
+        >
+          <Text
+            style={{
+              ...styles.font,
+              fontSize: 30,
+              padding: 20,
+              textAlign: 'center',
+            }}
+          >
+            {entities.decode(currentQuestion.question)}
+          </Text>
+        </Animated.View>
+        <View
+          style={[
+            styles.font,
+            {
+              marginTop: 20,
+              flex: 1,
+              flexDirection: 'row',
+            },
+          ]}
+        >
+          <View
+            style={{
+              margin: 30,
+            }}
+          >
+            <Icon
+              raised
+              name="thumbs-up"
+              type="font-awesome"
+              color="#63cdda"
+              reverse
+              size={30}
+              onPress={() => {
+                this.nextQuestion('True', componentId);
+              }}
+            />
+          </View>
+          <View
+            style={{
+              margin: 30,
+            }}
+          >
+            <Icon
+              raised
+              name="thumbs-down"
+              type="font-awesome"
+              color="#e15f41"
+              size={30}
+              reverse
+              onPress={() => {
+                this.nextQuestion('False', componentId);
+              }}
+            />
+          </View>
+        </View>
       </View>
-      <View
-        style={{
-          margin: 30,
-        }}
-      >
-        <Icon
-          raised
-          name="thumbs-down"
-          type="font-awesome"
-          color="#e15f41"
-          size={30}
-          reverse
-          onPress={() => {
-            nextQuestion('False', componentId);
-          }}
-        />
-      </View>
-    </View>
-  </View>
-);
+    );
+  }
+}
 
 QuestionScreen.propTypes = {
   componentId: PropTypes.string.isRequired,
